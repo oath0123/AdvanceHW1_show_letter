c8d27d16c2a0fd74e4d60ef14b6d34a2418b1bb7
TUF GAMING <oath0123@gmail.com>
2019-12-27 15:09:32 +0800

    Initial commit

---
+++ lib/src/main/java/com/example/lib/MyClass.java
@@ -0,0 +1,15 @@
+package com.example.lib;
+
+public class MyClass {
+    public static void main(String[] args) {   //參考https://zhidao.baidu.com/question/503176268.html
+        System.out.println();
+        for (int i = 65; i < 91; i++) {
+            System.out.print((char) i);
+        }
+        System.out.println();
+        for (int i = 97; i < 123; i++) {
+            System.out.print((char) i);
+        }
+        System.out.println();
+    }
+}
